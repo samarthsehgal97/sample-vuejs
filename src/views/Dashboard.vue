ea80cba3dc66299dc95a00b39b23716e3e10442b
Abhishek Tiwari <abhishek.twr@gmail.com>
2020-02-07 09:12:37 +1100

    Updated settings and passwor urls

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,10 +5,10 @@
       <template v-slot:lead>You are now logged in.</template>
       <hr class="my-4" />
       <p>You can update your profile details or password.</p>
-      <b-button variant="primary" href="#" class="mr-1">
+      <b-button variant="primary" :href="get_user_settings_url" target="_blank"  class="mr-1">
         <b-icon icon="gear-wide-connected"></b-icon> Update profile
       </b-button>
-      <b-button variant="danger" href="#" class="ml-1">
+      <b-button variant="danger" :href="get_user_password_url" target="_blank" class="ml-1">
         <b-icon icon="shield-lock"></b-icon> Change password
       </b-button>
     </b-jumbotron>
@@ -21,6 +21,12 @@ export default {
       return this.$auth.session.id_payload.given_name
         ? this.$auth.session.id_payload.given_name
         : "";
+    },
+    get_user_password_url() {
+      return this.$auth.get_user_password_url;
+    },
+    get_user_settings_url() {
+      return this.$auth.get_user_settings_url;
     }
   }
 };
